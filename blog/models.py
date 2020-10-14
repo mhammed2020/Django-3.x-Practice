33cb31033251271b3e41b271ed85e663169683b1
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-14 13:42:47 +0100

    Canonical URLs for models using get_absolute_url function

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 # Creating model managers
 
 class PublishedManager(models.Manager):
@@ -29,13 +29,23 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
     choices=STATUS_CHOICES,
     default='draft')
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+            self.publish.month,
+            self.publish.day, self.slug])
+
     class Meta:
       ordering = ('-publish',)
     def __str__(self):
        return self.title
 
 
-
 # ...model manager 
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+
+#Canonical URLs for models
+    
+
+    
