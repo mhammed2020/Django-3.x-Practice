5224cbe40378e8dad610dff9c0bf827fc821641d
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-14 12:50:55 +0100

    Customizing the way that models are

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Post
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
