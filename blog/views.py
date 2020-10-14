5113699450af15aa5b4f72f86ce2ff109fe73701
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-14 18:48:44 +0100

    Add pagination to templates

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,27 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Post
     
+
+# pagination 
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def post_list(request):
-    posts = Post.published.all()
+    # posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) 
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
     'blog/post/list.html',
-    {'posts': posts})
+    {    'page': page,
+        'posts': posts
+    })
 
 
 
