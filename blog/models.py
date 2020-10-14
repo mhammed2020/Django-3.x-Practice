96598556a2af083cd6b86cb71144fcf06b002ba7
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-14 13:23:04 +0100

    create the first view in order to write our logic

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+# Creating model managers
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -27,3 +33,9 @@ class Post(models.Model):
       ordering = ('-publish',)
     def __str__(self):
        return self.title
+
+
+
+# ...model manager 
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+    
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+    'blog/post/list.html',
+    {'posts': posts})
+
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+status='published',
+publish__year=year,
+publish__month=month,
+publish__day=day)
+
+    return render(request,
+    'blog/post/detail.html',
+    {'post': post})
