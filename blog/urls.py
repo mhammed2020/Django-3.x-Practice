4a2d352af617480a0f1a36968fb275fb6fca41df
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-14 18:57:56 +0100

    Create objects, and pagination using CBVs

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,9 @@ app_name = 'blog'
 
 urlpatterns = [
 # post views
-path('', views.post_list, name='post_list'),
+# path('', views.post_list, name='post_list'),
+
+path('', views.PostListView.as_view(), name='post_list'),
 path('<int:year>/<int:month>/<int:day>/<slug:post>/',
 views.post_detail,
 name='post_detail'),
--- blog/views.py
+++ blog/views.py
@@ -40,3 +40,13 @@ publish__day=day)
     return render(request,
     'blog/post/detail.html',
     {'post': post})
+
+
+# Using class-based views .. an other logic .. fast
+
+from django.views.generic import ListView
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
