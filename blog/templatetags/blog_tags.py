d8bf6ca76a1352a645caaed928a56e275286c662
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-26 11:47:43 +0100

    add most commented posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -9,3 +9,11 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+from django.db.models import Count
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+    total_comments=Count('comments')
+).order_by('-total_comments')[:count]
